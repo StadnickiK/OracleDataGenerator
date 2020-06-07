7670c9d663776e0e6ae7a21785e043c0b979f19e
StadnickiK <66563003+StadnickiK@users.noreply.github.com>
2020-06-07 20:11:14 +0200

    Add files via upload

---
+++ Connection/OracleDB.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+using SklepGenerator.OracleDataManager;
+
+namespace SklepGenerator.Connection
+{
+    public class OracleDB
+    {
+        public OracleConnection OConnection { get; set; }
+        public string ConString { get; set; }
+
+        DataGenerator DataGenerator = new DataGenerator();
+        DataLoader DataLoader { get; set; }
+        FileManager _fileManager = new FileManager();
+        public DataInserter DataInserter { get; set; }
+
+        public OracleDB(string conString) {
+            ConString = conString;
+        }
+
+        public void Connect()
+        {
+            OConnection = new OracleConnection
+            {
+                ConnectionString = ConString
+            };
+            OConnection.Open();
+            this.DataLoader = new DataLoader(OConnection);
+            DataInserter = new DataInserter(OConnection);
+            Console.WriteLine("Connected to Oracle " + OConnection.ServerVersion);
+        }
+        public Dictionary<string, Table> GetDataScheme() { return DataLoader.DataScheme; }
+
+        public void Connect(string conString)
+        {
+            OConnection = new OracleConnection
+            {
+                ConnectionString = conString
+            };
+            OConnection.Open();
+            Console.WriteLine("Connected to Oracle " + OConnection.ServerVersion);
+        }
+
+        public void Close()
+        {
+            OConnection.Close();
+            OConnection.Dispose();
+            Console.WriteLine("Connection closed");
+        }
+
+        public void LoadData()
+        {
+            DataLoader.LoadDB();
+        }
+
+        public int GetLastID(string tableName)
+        {
+            return DataLoader.GetLastID(tableName);
+        }
+
+        public List<int> GetUnusedIDs(string tableName)
+        {
+            return DataLoader.GetUnusedRowsIds(tableName);
+        }
+
+        public List<int> GetUnusedIDsOfRelations(string tableName)
+        {
+            return DataLoader.GetUnusedIDsOfRelations(tableName);
+        }
+
+
+        public int InsertRandValues(string tableName)
+        {
+            Table t = DataLoader.DataScheme[tableName];
+            string data = "";
+            int i = 0;
+            int id = 0;
+            foreach(DataTypes.DataType d in t.Columns)
+            {
+                DateTime date = new DateTime(1970,1,1);
+                if (d.Value == "DATE")
+                {
+                    date = DataLoader.GetLastDate(tableName, d.Name);
+                }
+                if (t.Relations.ContainsKey(d.Name))
+                {
+                    if(DataLoader.DataScheme[t.Relations[d.Name]].FreeIDs.Count != 0)
+                    {
+                        int freeID = DataLoader.DataScheme[t.Relations[d.Name]].FreeIDs[DataGenerator.GetRandNumber(0,
+                            DataLoader.DataScheme[t.Relations[d.Name]].FreeIDs.Count)];
+                        DataLoader.DataScheme[t.Relations[d.Name]].FreeIDs.Remove(freeID);
+                        data += freeID;
+                    }
+                    else
+                    {
+                        data += InsertRandValues(t.Relations[d.Name]).ToString();
+                    }
+                }
+                else
+                {
+
+                    if (i == t.Columns.Count - 1)
+                    {
+                        data += DataGenerator.GenerateRandData(d.Value, date);
+                    }
+                    else if (i == 0)
+                    {
+                        id = GetLastID(tableName);
+                        id++;
+                        data += id.ToString();
+                    }
+                    else
+                    {
+                        data += DataGenerator.GenerateRandData(d.Value, date);
+                    }
+                }
+                if (i != t.Columns.Count - 1)
+                    data += ",";
+                i++;
+            }
+            string s = DataInserter.InsertData(data, tableName);
+            Console.WriteLine(s);
+            _fileManager.AddLine(s);
+            return id;
+        }
+
+        public int InsertRandValues(string tableName, int count)
+        {
+            Console.WriteLine("Inserting " + count + " rows into " + tableName);
+            for (int i = 0; i < count;i++)
+            {
+                InsertRandValues(tableName);
+            }
+            Console.WriteLine("Inserting finished");
+            _fileManager.SaveToFile(tableName);
+            return 0;
+        }
+
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace SklepGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(392, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(88, 65);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Rows number";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(214, 65);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(172, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "Table";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(9, 92);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(0, 0);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(273, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Login";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(167, 25);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(61, 25);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Login:                         Password:\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "DataGenerator";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
